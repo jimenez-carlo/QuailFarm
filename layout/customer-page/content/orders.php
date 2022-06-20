a663e3f57ef215e1a22e9a5ba842c70d485a9207
Carlo Jimenez <jimenez.carlo.llabor@gmail.com>
2022-06-20 14:38:54 +0800

    Update orders.php

--- layout/customer-page/content/orders.php
+++ layout/customer-page/content/orders.php
@@ -1,67 +1,69 @@
 <div class="container mt-3">
   <h2><i class="fa fa-archive"></i> Orders</h2>
-  <?php foreach ($data['orders']['invoice'] as $res => $invoice_key) { ?>
-    <div class="col-12 mb-3">
-      <div class="card">
-        <div class="card-header bg-dark text-warning">
-          <i class="fa fa-archive"></i> #<?php echo $res; ?>
-        </div>
-        <div class="card-body">
-          <table class="table table-sm table-striped table-hover table-bordered" style="width:100%">
-            <thead class="table-dark">
-              <tr>
-                <th scope="col">ID#</th>
-                <th scope="col">Status</th>
-                <th scope="col">Last Updated</th>
-                <th scope="col">Product Name</th>
-                <th scope="col">Product Price</th>
-                <th scope="col">Qty</th>
-                <th scope="col">Total Price</th>
-                <th scope="col"></th>
-              </tr>
-            </thead>
-            <tbody>
-              <?php
-              $id = 1;
-              $price = 0;
-              $total_price = 0;
-              $qty = 0; ?>
-              <?php foreach ($data['orders']['invoice'][$res] as $sub_res) { ?>
+  <?php if (isset($data['orders']['invoice'])) { ?>
+    <?php foreach ($data['orders']['invoice'] as $res => $invoice_key) { ?>
+      <div class="col-12 mb-3">
+        <div class="card">
+          <div class="card-header bg-dark text-warning">
+            <i class="fa fa-archive"></i> #<?php echo $res; ?>
+          </div>
+          <div class="card-body">
+            <table class="table table-sm table-striped table-hover table-bordered" style="width:100%">
+              <thead class="table-dark">
                 <tr>
-                  <td><?php echo $id++; ?></td>
-                  <td><?php echo ucwords($sub_res['status']); ?></td>
-                  <td><?php echo $sub_res['date_updated']; ?></td>
-                  <td><?php echo $sub_res['name']; ?></td>
-                  <td class="text-end"><?php echo number_format($sub_res['product_price'], 2); ?></td>
-                  <td class="text-end"><?php echo $sub_res['qty']; ?></td>
-                  <td class="text-end"><?php echo number_format($sub_res['product_price'] * $sub_res['qty'], 2); ?></td>
-                  <td class="text-end" style="width: 0.1%;">
-                    <?php if ($sub_res['status_id'] == 2) { ?>
-                      <form method="" name="update_transaction">
-                        <input type="hidden" name="id" value="<?php echo $sub_res['id']; ?>">
-                        <input type="hidden" name="status" value="5">
-                        <button type="submit" class="btn btn-sm btn-warning"> Cancel</button>
-                      </form>
-                    <?php } ?>
-                  </td>
+                  <th scope="col">ID#</th>
+                  <th scope="col">Status</th>
+                  <th scope="col">Last Updated</th>
+                  <th scope="col">Product Name</th>
+                  <th scope="col">Product Price</th>
+                  <th scope="col">Qty</th>
+                  <th scope="col">Total Price</th>
+                  <th scope="col"></th>
                 </tr>
-                </td>
-                <?php $price +=       (in_array(intval($sub_res['status_id']), array(1, 5, 6))) ? 0 :  $sub_res['product_price']; ?>
-                <?php $total_price += (in_array(intval($sub_res['status_id']), array(1, 5, 6))) ? 0 :  $sub_res['product_price'] * $sub_res['qty']; ?>
-                <?php $qty +=         (in_array(intval($sub_res['status_id']), array(1, 5, 6))) ? 0 : $sub_res['qty']; ?>
-              <?php } ?>
-              <tr>
-                <td colspan="3">Total</td>
-                <td id="total_price" class="text-end"><?php echo number_format($price, 2); ?></td>
-                <td id="total_qty" class="text-end"><?php echo $qty; ?></td>
-                <td id="total_final_price" class="text-end"><?php echo number_format($total_price, 2); ?></td>
-                <td></td>
-              </tr>
-            </tbody>
-          </table>
+              </thead>
+              <tbody>
+                <?php
+                $id = 1;
+                $price = 0;
+                $total_price = 0;
+                $qty = 0; ?>
+                <?php foreach ($data['orders']['invoice'][$res] as $sub_res) { ?>
+                  <tr>
+                    <td><?php echo $id++; ?></td>
+                    <td><?php echo ucwords($sub_res['status']); ?></td>
+                    <td><?php echo $sub_res['date_updated']; ?></td>
+                    <td><?php echo $sub_res['name']; ?></td>
+                    <td class="text-end"><?php echo number_format($sub_res['product_price'], 2); ?></td>
+                    <td class="text-end"><?php echo $sub_res['qty']; ?></td>
+                    <td class="text-end"><?php echo number_format($sub_res['product_price'] * $sub_res['qty'], 2); ?></td>
+                    <td class="text-end" style="width: 0.1%;">
+                      <?php if ($sub_res['status_id'] == 2) { ?>
+                        <form method="" name="update_transaction">
+                          <input type="hidden" name="id" value="<?php echo $sub_res['id']; ?>">
+                          <input type="hidden" name="status" value="5">
+                          <button type="submit" class="btn btn-sm btn-warning"> Cancel</button>
+                        </form>
+                      <?php } ?>
+                    </td>
+                  </tr>
+                  </td>
+                  <?php $price +=       (in_array(intval($sub_res['status_id']), array(1, 5, 6))) ? 0 :  $sub_res['product_price']; ?>
+                  <?php $total_price += (in_array(intval($sub_res['status_id']), array(1, 5, 6))) ? 0 :  $sub_res['product_price'] * $sub_res['qty']; ?>
+                  <?php $qty +=         (in_array(intval($sub_res['status_id']), array(1, 5, 6))) ? 0 : $sub_res['qty']; ?>
+                <?php } ?>
+                <tr>
+                  <td colspan="3">Total</td>
+                  <td id="total_price" class="text-end"><?php echo number_format($price, 2); ?></td>
+                  <td id="total_qty" class="text-end"><?php echo $qty; ?></td>
+                  <td id="total_final_price" class="text-end"><?php echo number_format($total_price, 2); ?></td>
+                  <td></td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
-    </div>
+    <?php } ?>
   <?php } ?>
 </div>
 </div>
